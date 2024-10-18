288658afd6e7244d295e7c5069cc0bcd0d637039
FrostyKitten02 <alen.fridau02@gmail.com>
2024-10-18 17:35:06 +0200

    initial commit

---
+++ src/test/java/si/afridau/emailsender/EmailsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package si.afridau.emailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailsenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
